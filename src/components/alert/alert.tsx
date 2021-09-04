f1b405dae5ee9ae6d3ee6043a484145aab4956f5
Alex Margolin <csight@msn.com>
2021-09-04 09:07:26 +0300

    [alert] initial structure

---
+++ src/components/alert/alert.tsx
@@ -0,0 +1,37 @@
+import { AlertProps } from "./"
+import { forwardRef } from "react"
+import { makeClasses } from "@/hooks"
+import modules from "./alert.module.scss"
+
+const classes = makeClasses(modules)
+
+export const classNames = {
+  root: "alert",
+}
+
+/**
+ * Alert Component
+ *
+ * @since 1.0.0
+ */
+const Alert = forwardRef<HTMLDivElement, AlertProps>(
+  (props, forwardedRef) => {
+    const { className, type = "info", ...rest } = props
+
+    const attrClasses = {
+      [`alert--${type}`]: !!type,
+    }
+
+    return (
+      <div
+        ref={forwardedRef}
+        className={classes(classNames.root, className, attrClasses)}
+        {...rest}
+      >
+        test
+      </div>
+    )
+  },
+)
+
+export default Alert
---
+++ src/components/alert/index.ts
@@ -0,0 +1,7 @@
+import { ComponentPropsWithRef } from "react"
+
+export type AlertType = "info" | "error" | "success"
+
+export interface AlertProps extends ComponentPropsWithRef<"div"> {
+  type?: AlertType
+}
